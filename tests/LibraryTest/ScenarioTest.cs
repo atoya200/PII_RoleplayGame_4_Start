527152b857681eba8f6c9c83bbd4e8f1542f8e82
atoya200 <agustintoya2004@gmail.com>
2022-10-31 10:29:00 -0300

    Preparado para hacer los tests

---
+++ tests/LibraryTest/ScenarioTest.cs
@@ -0,0 +1,20 @@
+using RoleplayGame.Items;
+using RoleplayGame.Characters;
+using RoleplayGame.Encounters;
+using RoleplayGame.Scenarios;
+using NUnit.Framework;
+
+namespace LibraryTest;
+[TestFixture]
+public class ScenarioTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Scenario()
+    {
+    }
+}
